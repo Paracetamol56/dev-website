c3daf4fc569c06aedd9cc239e92ae085ca0f69f5
Mathéo <matheo.galu56@gmail.com>
2023-09-27 21:01:27 +0200

    ✨ Main page

---
+++ src/routes/hertzsprung-russell-diagram/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+	import Diagram from "./Diagram.svelte";
+
+  export let data: PageData;
+</script>
+
+<div class="container mx-auto">
+  <h1 class="mt-16 mb-8 text-6xl font-semibold text-center">
+    Herzsprung-Russell diagram
+  </h1>
+  <Diagram data={data.stars} />
+</div>
