6a33bf085c1453b9f582014e326d727baf3be92e
Mathéo <matheo.galu56@gmail.com>
2024-03-15 14:42:35 +0100

    🏷️ Fix tag overflow on PageDisplay

--- src/routes/PageDisplay.svelte
+++ src/routes/PageDisplay.svelte
@@ -7,11 +7,14 @@
 
 <div class="p-8 bg-ctp-crust/50 backdrop-blur-sm rounded-md shadow-md shadow-ctp-crust z-10">
 	<div class="mb-2 flex justify-left flex-wrap gap-x-2 items-center">
-		{#each page.tags as tag}
+		{#each page.tags.slice(0, 4) as tag}
 			<a href="{path}?tag={tag}">
 				<span class="text-sm font-semibold text-ctp-lavender">#{tag}</span>
 			</a>
 		{/each}
+		{#if page.tags.length > 4}
+			<span class="text-sm font-semibold text-ctp-lavender">...</span>
+		{/if}
 	</div>
 	<a href="{path}/{page.slug}">
 		<h4 class="mb-4 text-2xl font-bold hover:opacity-75 transition-opacity">
